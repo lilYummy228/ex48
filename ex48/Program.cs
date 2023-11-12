b0afd83c890c70c4f6b7613e6fd2508bca53d51f
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-11-12 13:11:26 +0500

    123

--- ex48/Program.cs
+++ ex48/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ex48
 {
@@ -10,200 +7,254 @@ namespace ex48
     {
         static void Main(string[] args)
         {
-            const string CommandCreateSquad = "1";
-            const string CommandStartBattle = "2";
+            Battlefield battlefield = new Battlefield();
 
-            World world = new World();
+            battlefield.Battle();
 
-            Console.Write($"Война\n\n{CommandCreateSquad} - создать отряды\n{CommandStartBattle} - начать битву\n\nВаш ввод: ");
+            battlefield.ShowBattleResult();
+        }
+    }
 
-            switch (Console.ReadLine())
+    class Battlefield
+    {
+        private Platoon _firstCountry = new Platoon();
+        private Platoon _secondCountry = new Platoon();
+        private Soldier _firstSoldier;
+        private Soldier _secondSoldier;
+
+        public void Battle()
+        {
+            while (_firstCountry.GetCount() > 0 && _secondCountry.GetCount() > 0)
             {
-                case CommandCreateSquad:
-                    world.CreateArmy();
-                    break;
+                _firstSoldier = _firstCountry.GetSoldierFromPlatoon();
+                _secondSoldier = _secondCountry.GetSoldierFromPlatoon();
 
-                case CommandStartBattle:
-                    break;
+                ShowPlatoons();
+                Fight();
+                DetermineWinner();
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public void ShowBattleResult()
+        {
+            if (_firstCountry.GetCount() < 0 && _secondCountry.GetCount() < 0)
+            {
+                Console.WriteLine("Ничья");
+            }
+            else if (_firstCountry.GetCount() <= 0)
+            {
+                Console.WriteLine("Победа за вторым взводом");
+            }
+            else if (_secondCountry.GetCount() <= 0)
+            {
+                Console.WriteLine("Победа за первым взводом");
+            }
+        }
+
+        private void ShowPlatoons()
+        {
+            Console.WriteLine("Взвод первой страны: ");
+            _firstCountry.ShowPlatoon();
+            Console.WriteLine("Взвод второй страны: ");
+            _secondCountry.ShowPlatoon();
+        }
+
+        private void Fight()
+        {
+            _firstSoldier.UseAnAttack();
+            _secondSoldier.UseAnAttack();
+            _firstSoldier.TakeDamage(_secondSoldier.Damage);
+            _secondSoldier.TakeDamage(_firstSoldier.Damage);
+        }
+
+        private void DetermineWinner()
+        {
+            if (_firstSoldier.Health <= 0)
+            {
+                _firstCountry.RemoveDiedSoldier(_firstSoldier);
+            }
+            else if (_secondSoldier.Health <= 0)
+            {
+                _secondCountry.RemoveDiedSoldier(_secondSoldier);
             }
         }
     }
 
-    class World
+    class Platoon
     {
-        private List<Soldier> _soldiers;
+        private List<Soldier> _soldiers = new List<Soldier>();
+        private Random _random = new Random();
 
-        public World()
+        public Platoon()
         {
-            _soldiers = new List<Soldier>
-            {
-                new Sniper("Снайпер", 100, 150),
-                new Pyro("Поджигатель", 400, 100),
-                new Medic("Медик", 150, 50),
-                new MachineGunner("Пулеметчик", 600, 50),
-                new Bomber("Подрыватель", 250, 150),
-                new Engineer("Инжинер", 200, 100)
-            };
+            CreatePlatoon(_soldiers, GetSoldierCount());
         }
 
-        public void CreateArmy()
+        public void RemoveDiedSoldier(Soldier soldier)
         {
-            List<Soldier> army = new List<Soldier>();
-            bool isEmpty = true;
+            _soldiers.Remove(soldier);
+        }
 
-            while (isEmpty)
+        public void ShowPlatoon()
+        {
+            foreach (Soldier soldier in _soldiers)
             {
-                Console.Clear();
-
-                for (int i = 0; i < _soldiers.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {_soldiers[i].Name}");
-                }
-
-                Console.Write("Кого вы хотите добавить во взвод? ");
-
-                if (int.TryParse(Console.ReadLine(), out int index))
-                {
-                    army.Add(_soldiers[index - 1]);
-                }
-
-                if (army.Count > 5)
-                {
-                    isEmpty = false;
-                }
+                soldier.ShowInfo();
             }
 
-            Console.WriteLine("Взвод номер один");
+            Console.WriteLine();
+        }
 
-            foreach (Soldier soldier in army)
+        public int GetCount()
+        {
+            return _soldiers.Count;
+        }
+
+        public Soldier GetSoldierFromPlatoon()
+        {
+            return _soldiers[_random.Next(0, _soldiers.Count)];
+        }
+
+        private void CreatePlatoon(List<Soldier> soldiers, int soldierCount)
+        {
+            for (int i = 0; i < soldierCount; i++)
             {
-                Console.WriteLine($"{soldier.Name}");
+                soldiers.Add(GetSoldier());
+            }
+        }
+
+        private int GetSoldierCount()
+        {
+            int minSoldierCount = 3;
+            int maxSoldierCount = 6;
+            int soldierCount = _random.Next(minSoldierCount, maxSoldierCount);
+            return soldierCount;
+        }
+
+        private Soldier GetSoldier()
+        {
+            int maxSoldierCount = 4;
+            int randomSoldierIndex = _random.Next(maxSoldierCount);
+
+            if (randomSoldierIndex == 0)
+            {
+                return new Sniper("Снайпер", 500, 100);
+            }
+            else if (randomSoldierIndex == 1)
+            {
+                return new Miner("Минер", 800, 100);
+            }
+            else if (randomSoldierIndex == 2)
+            {
+                return new MachineGunner("Пулеметчик", 1100, 50);
+            }
+            else
+            {
+                return new Spy("Шпион", 600, 50);
             }
         }
     }
 
     class Soldier
     {
+        public string Name { get; protected set; }
+        public int Health { get; protected set; }
+        public int Damage { get; protected set; }
+        public int InitialDamage { get; protected set; }
+
         public Soldier(string name, int health, int damage)
         {
             Name = name;
             Health = health;
             Damage = damage;
+            InitialDamage = Damage;
         }
-
-        public string Name { get; protected set; }
-        public int Health { get; protected set; }
-        public int Damage { get; protected set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"{Name}. Здоровье: {Health}\nУрон: {Damage}");
+            Console.WriteLine($"{Name}, Здоровье: {Health}, урон: {Damage}");
         }
 
         public void TakeDamage(int damage)
         {
             Health -= damage;
+            Console.WriteLine($"\n{Name} нанес {damage} урона");
         }
 
-        public virtual void UseAbility() { }
+        public void UseAnAttack()
+        {
+            Damage = InitialDamage;
+
+            Random random = new Random();
+            int chance = 1;
+            int chanceValue = 4;
+            int randomValue = random.Next(chanceValue);
+
+            if (chance == randomValue)
+            {
+                Console.WriteLine();
+                UseAttack();
+            }
+        }
+
+        protected virtual void UseAttack() { }
     }
 
     class Sniper : Soldier
     {
-        private int _critDamage;
-        private int _initialDamage;
-        private int _critMultiplier = 2;
-        private int _critDamageValue = 0;
-        private int _critDamageChance = 4;
-        private Random _random = new Random();
+        public Sniper(string name, int health, int damage) : base(name, health, damage) { }
 
-        public Sniper(string name, int health, int damage) : base(name, health, damage)
+        protected override void UseAttack()
         {
-            Damage = _initialDamage;
-            _critDamage = Damage * _critMultiplier;
-        }
-
-        public override void UseAbility()
-        {
-            HeadShot();
-        }
-
-        public void HeadShot()
-        {
-            int chance = _random.Next(_critDamageChance);
-            Damage = _initialDamage;
-
-            if (chance == _critDamageValue)
-                Damage = _critDamage;
+            int extraDamage = 3;
+            Damage *= extraDamage;
+            Console.WriteLine("Попадание в голову!");
         }
     }
 
-    class Pyro : Soldier
+    class Miner : Soldier
     {
-        private int _fireDamage = 10;
-        public Pyro(string name, int health, int damage) : base(name, health, damage) { }
+        public Miner(string name, int health, int damage) : base(name, health, damage) { }
 
-        public override void UseAbility()
+        protected override void UseAttack()
         {
-            Burn();
-        }
-
-        public void Burn()
-        {
-            Damage += _fireDamage;
-        }
-    }
-
-    class Medic : Soldier
-    {
-        public Medic(string name, int health, int damage) : base(name, health, damage) { }
-
-        public override void UseAbility()
-        {
-
-        }
-
-        public void Heal()
-        {
-
-        }
-    }
-
-    class Bomber : Soldier
-    {
-        public Bomber(string name, int health, int damage) : base(name, health, damage) { }
-
-        public override void UseAbility()
-        {
-
+            int extraDamage = 50;
+            Damage += extraDamage;
+            Console.WriteLine("Противник наступил на мину!");
         }
     }
 
     class MachineGunner : Soldier
     {
-        private int _attackSpeed = 0;
+        private int _warmup = 1;
 
         public MachineGunner(string name, int health, int damage) : base(name, health, damage) { }
 
-        public override void UseAbility()
+        protected override void UseAttack()
         {
-            Overclocking();
-        }
-
-        public void Overclocking()
-        {
-            Damage += _attackSpeed;
-            _attackSpeed += 10;
+            int extraDamage = 20;
+            Damage += _warmup * extraDamage;
+            _warmup++;
+            Console.WriteLine("Пулемет разгоняется!");
         }
     }
 
-    class Engineer : Soldier
+    class Spy : Soldier
     {
-        public Engineer(string name, int health, int damage) : base(name, health, damage) { }
+        public Spy(string name, int health, int damage) : base(name, health, damage) { }
 
-        public override void UseAbility()
+        protected override void UseAttack()
         {
-
+            Random random = new Random();
+            int minRandomValue = -10;
+            int maxRandomValue = 100;
+            int randomValue = random.Next(minRandomValue, maxRandomValue);
+            Health += randomValue;
+            Damage += randomValue;
+            Console.WriteLine("Шпион получил преимущество над противником!");
         }
     }
 }
